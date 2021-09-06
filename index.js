a7ae5fa84ebff7ba4910495ee500d2852d4d0a94
Rafael Varela <rafaelvrcr@gmail.com>
2021-09-06 11:35:32 -0300

    tentar do zero dinovo

--- index.js
+++
@@ -1,8 +0,0 @@
-function userInput()
-{
-var login = document.getElementById("login");
-var senha = document.getElementById("senha");
-alert("Seu login é:" + login+ " e sua senha é:" + senha);
-}
-
-document.getElementById("btn").addEventListener("click", userInput())
